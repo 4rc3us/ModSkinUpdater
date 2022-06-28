d27015f7ba81ca92011737c252a2b10c14f04776
4rc3us <juandanielarce398@gmail.com>
2022-06-28 12:24:40 -0500

    Implement constant HOME to avoid multiple calls to GetHomeFolder script

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ import wget
 import zipfile
 import subprocess, sys
 
+p = subprocess.Popen(
+    ["powershell.exe", ".\\Scripts\\GetHomeFolder.ps1"], stdout=subprocess.PIPE
+)
+
+HOME = p.communicate()[0].decode("utf-8")[0:-2]
+
 
 def unZip():
-    p = subprocess.Popen(
-        ["powershell.exe", ".\\GetHomeFolder.ps1"], stdout=subprocess.PIPE
-    )
-
-    pathContenedor = p.communicate()[0].decode("utf-8")[0:-2]
-    fileName = os.listdir(pathContenedor)
-    pathWfile = pathContenedor + "\\" + fileName[0]
+    fileName = os.listdir(HOME)
+    pathWfile = HOME + "\\" + fileName[0]
 
     with zipfile.ZipFile(pathWfile, "r") as zip_ref:
-        zip_ref.extractall(pathContenedor)
+        zip_ref.extractall(HOME)
 
-    print("\n\nArchivo descomprimido")
+    print("\n\nunzipped file")
 
 
 def getUrl():
@@ -30,35 +31,25 @@ def getUrl():
 
 
 def createShortcut():
-    p = subprocess.Popen(["powershell.exe", ".\\CreateShortCut.ps1"], stdout=sys.stdout)
+    p = subprocess.Popen(["powershell.exe", ".\\Scripts\\CreateShortCut.ps1"], stdout=sys.stdout)
     p.communicate()
 
 
 if os.system('mkdir "%USERPROFILE%\ModSkin"') == 0:
-    p = subprocess.Popen(
-        ["powershell.exe", ".\\GetHomeFolder.ps1"], stdout=subprocess.PIPE
-    )
-    output = p.communicate()[0].decode("utf-8")[0:-2]
     url = getUrl()
     print("\nDescargando archivo", url + "\n")
-    wget.download(url, output)
+    wget.download(url, HOME)
     unZip()
     createShortcut()
 
 else:
     os.system('rmdir "%USERPROFILE%\ModSkin" /S /Q')
 
-    p = subprocess.Popen(
-        ["powershell.exe", ".\\GetHomeFolder.ps1"], stdout=subprocess.PIPE
-    )
-
-    output = p.communicate()[0].decode("utf-8")[0:-2]
-
     print("\nEliminando...")
 
     if os.system('mkdir "%USERPROFILE%\ModSkin"') == 0:
         url = getUrl()
         print("\nDescargando archivo", url, "\n")
-        wget.download(url, output)
+        wget.download(url, HOME)
         unZip()
         createShortcut()
